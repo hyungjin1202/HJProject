1e09da47da8fadb4e3b2efa1efa031df397d0c98
hyungjin1202 <gudwlsdyd10@naver.com>
2024-09-28 18:39:35 +0900

    Unity

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
+using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour
 {
@@ -9,63 +11,43 @@ public class GameManager : MonoBehaviour
     public static GameManager GetInstance()
     {
         if(_instance == null)
-        {
+        {   // 씬에서 찾아 보자
             _instance = FindObjectOfType<GameManager>();
         }
+        // 그런데도 없어 ?
+        if (_instance == null) 
+        {
+            GameObject go = new GameObject("GameManager");
+            _instance = go.AddComponent<GameManager>();
+            
+        }
+
 
         return _instance;
     }
 
-
-    public Canvas canva;
-
-
- 
     void Start()
     {
-        
 
         Debug.Log(" 시작 하는거 확인");
-        CreateStartPage();
+        UIManager.Instance.CreateGameUI<UIStartPage>();
+
     }
 
-    private void Update()
+    public void GameStart()
     {
-        /*if(Input.GetKeyDown(KeyCode.A))
-        {
-            // 이런것이 있는지 불러와본다.
-            GameObject uiPage = Resources.Load<GameObject>("Prefab/UIResultPage");
-
-            Debug.Log(uiPage); // Null
-
-            GameObject ui = Instantiate(uiPage);
-
-            ui.transform.SetParent(canva.transform);
-            (ui.transform as RectTransform).offsetMin = Vector3.zero;
-            (ui.transform as RectTransform).offsetMax = Vector3.zero;
-        }*/
 
     }
-
-
-    private void CreateStartPage()
+    public void ChanegGameScene()
     {
-        // 이런것이 있는지 불러와본다.
-        GameObject uiPage = Resources.Load<GameObject>("Prefab/UIStartPage");
+        UIManager.Instance.RemovePage<UISelectMode>();
 
-        Debug.Log(uiPage); // Null
+        SceneManager.LoadScene("InGameScene");
 
-        GameObject ui = Instantiate(uiPage);
-
-        ui.transform.SetParent(canva.transform);
-
-        // 프로젝트마다 예외처리, 앵커처리를하기 위함
-        (ui.transform as RectTransform).offsetMin = Vector3.zero;
-        (ui.transform as RectTransform).offsetMax = Vector3.zero;
-
-        UIStartPage comp = ui.GetComponent<UIStartPage>();
-        comp.SetCanvas(canva);
+        CountTimer ui = UIManager.Instance.CreateGameUI<CountTimer>() as CountTimer;
+       // ui.SetGameTime();
     }
+    
 
     
 }
--- Assets/Scripts/UIStartPage.cs
+++ Assets/Scripts/UIStartPage.cs
@@ -2,9 +2,10 @@ using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class UIStartPage : MonoBehaviour
+public class UIStartPage : UIBase
 {
     private Canvas _canvas;
 
@@ -17,41 +18,14 @@ public class UIStartPage : MonoBehaviour
 
     void Start()
     {
-        Button startButton = GetComponentInChildren<Button>();
-
+        Button startButton = GetComponentInChildren<Button>();                                                                   
         startButton.onClick.AddListener(GameStart);
-
-        /*Text buttonText = GetComponentInChildren<Text>();
-        buttonText.text = "바뀐 텍스트";*/
-
-    }
-
-    private void Update()
-    {
-        float alpha = GetComponent<Image>().color.a;
-
-        GetComponent<Image>().color = new Color(0,0,0, alpha + Time.deltaTime );
-    }
+    }   
 
     public void GameStart()
     {
-        Debug.Log("게임 시작");
-
-        // 이런것이 있는지 불러와본다.
-        GameObject uiPage = Resources.Load<GameObject>("Prefab/UIIngamePage");
-
-        Debug.Log(uiPage); // Null
-
-        GameObject ui = Instantiate(uiPage); // Scene 에 만들어져서  나왔다.
-
-        UIIngamePage comp = ui.GetComponent<UIIngamePage>();
-        comp.SetCanvas(_canvas);
-
-        ui.transform.SetParent(_canvas.transform);
-
-        (ui.transform as RectTransform).offsetMin = Vector3.zero;
-        (ui.transform as RectTransform).offsetMax = Vector3.zero;
-
+        UIManager.Instance.CreatePage<UISelectMode>();
+        UIManager.Instance.RemovePage<UIStartPage>();
     }
 
 }
